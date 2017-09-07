ad7aeb0ae3978b8901840268270ba0adfd048b66
Zahra Khan <zarssskhan@gmail.com>
2017-09-07 08:04:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, { [key]: value });
 };
 
-function
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  
+}
